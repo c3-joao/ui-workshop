43163606cf52ccce3e522afda1d8495f00a9d840
Jacob LeBlanc <jacob.leblanc@c3.ai>
2024-11-11 22:38:56 +0000

    convert to grid layout part 1

--- windTurbineUiApp/src/epics/UiSdlApplicationStateWT.ts
+++ windTurbineUiApp/src/epics/UiSdlApplicationStateWT.ts
@@ -33,7 +33,7 @@ export function storeEventRecordAction(id, obj) {
 export function heatMapFilterReducer(state, action) {
   const appStateId = action.payload.componentId;
   state = setConfigInApplicationState(appStateId, state, ['turbineFilter'], action.payload.obj.turbineFilter);
-  return setConfigInApplicationState(appStateId, state, ['eventFilter'], action.payload.obj.turbineFilter);
+  return setConfigInApplicationState(appStateId, state, ['eventFilter'], action.payload.obj.eventFilter);
 }
 
 export function filterHeatMapAction(id, obj) {
